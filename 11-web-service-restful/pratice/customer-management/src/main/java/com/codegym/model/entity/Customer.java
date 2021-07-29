dde45fd87e7199bb88ec4c13d618bd1e1443b2c7
Hau HP <hauhpdn2015@gmail.com>
2021-07-29 09:41:49 +0700

    th

---
+++ 11-web-service-restful/pratice/customer-management/src/main/java/com/codegym/model/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.codegym.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
